232c862acf56851c97f27bdad88b26cbe626d9c3
Crowdbotics <engineering@crowdbotics.com>
2024-01-03 18:22:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "juanprdwithoutcache-1",
+  url: "https://juanprdwithoutcache-1.botics.co",
+  api: "https://juanprdwithoutcache-1.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
